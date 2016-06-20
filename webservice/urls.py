ffc27c99a7ccf133244f0277a331dd95baccaf56
Ricardo Dantas <licensed@brasnet.org>
2016-06-20 12:05:20 -0300

    Edit project name to webservice

--- webservice/urls.py
+++ webservice/urls.py
@@ -1,4 +1,4 @@
-"""qacad URL Configuration
+"""webservice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
